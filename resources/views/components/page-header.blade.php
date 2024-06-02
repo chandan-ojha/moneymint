2b81c8cc23133af1217d75f1b994285a415d530e
Chandan Ojha <chandanojha776@gmail.com>
2024-06-02 17:02:57 +0600

    Update page-header.blade.php

--- resources/views/components/page-header.blade.php
+++ resources/views/components/page-header.blade.php
@@ -15,8 +15,8 @@
                     </div>
                 </div>
             </div>
-            <div class="mt-6 flex space-x-3 md:ml-4 md:mt-0">
-                <button type="button"
+            <div class="mt-6 flex space-x-3 md:ml-4 md:mt-0" x-data="{ open: false }">
+                <button @click="open = true" type="button"
                         class="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
                     Add Income
                 </button>
@@ -24,6 +24,7 @@
                         class="inline-flex items-center rounded-md bg-cyan-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600">
                     Add Expense
                 </button>
+                @include('components.forms.form')
             </div>
         </div>
     </div>
