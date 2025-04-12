5781e6e350e91fa1f732334b1ed3d6c83200d4ed
Chandan Ojha <chandanojha776@gmail.com>
2025-04-12 12:04:41 +0600

    Update layout.blade.php

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,16 +1,20 @@
 <!doctype html>
 <html lang="en" class="h-full bg-gray-100">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>MoneyMint</title>
     <!-- AlpineJS CDN -->
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
+
 <body class="h-full">
     {{ $slot }}
 </body>
+
 </html>
